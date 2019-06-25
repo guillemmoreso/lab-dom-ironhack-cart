a34e432fdfafd8724d7ad1ce6b116f91eaaa0138
Guillem Moreso <guillem.moreso@gmail.com>
2019-06-25 17:37:53 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,52 +1,72 @@
-function deleteItem(event){
+/*var price = document.getElementsByClassName("product-price");
+var input = document.getElementById("quantity").value;
+console.log(input);*/
 
+function updatePrice() {
+  let price = document.getElementsByClassName("product-price");
+  let total = 0;
+  let totalPrice = 0;
+  for (let i = 0; i < price.length; i++) {
+    const input = document.getElementsByClassName("quantity")[i].value;
+    let productPrice = parseFloat(price[i].innerText.replace("$", ""));
+    total = input * productPrice;
+    totalPrice += total;
+    let spanPrice = document.getElementsByClassName("product-total-price")[i];
+    spanPrice.innerHTML = "$" + total.toFixed(2);
+  }
+  console.log(totalPrice);
+  document.getElementById("totalPrice").innerHTML =
+    "Total: " + "$" + totalPrice;
+}
+let deleteButtons = document.getElementsByClassName("btn-delete");
+deleteButtons.addEvenListener("click", deleteProduct);
+
+function deleteProduct() {
+  let buttonClicked = event.target;
+  buttonClicked.parentElement.parentElement.remove();
+}
+function createProduct() {
+  let input1 = document.getElementById("name");
+  let input2 = document.getElementById("price");
+  let newProducts = document.getElementById("div-products");
+  newProducts.innerHTML = 
 }
 
-function getPriceByProduct(itemNode){
+function deleteItem(event) { }
 
-}
+function getPriceByProduct(itemNode) { }
 
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(productPrice, index) { }
 
-}
+function getTotalPrice() { }
 
-function getTotalPrice() {
+function createQuantityInput() { }
 
-}
+function createDeleteButton() { }
 
-function createQuantityInput(){
+function createQuantityNode() { }
 
-}
+function createItemNode(dataType, itemData) { }
 
-function createDeleteButton(){
+function createNewItemRow(itemName, itemUnitPrice) { }
 
-}
+function createNewItem() { }
 
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+/*
+window.onload = function() {
+  var calculatePriceButton = document.getElementById("calc-prices-button");
+  var createItemButton = document.getElementById("new-item-create");
+  var deleteButtons = document.getElementsByClassName("btn-delete");
 
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
 };
+*/
+Collapse
+
+
+
